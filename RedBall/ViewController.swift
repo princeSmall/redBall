0c3e026ff4c459df753dde65f827d2e7b9ab646e
Tongle <Tongle@sansi.com>
2017-08-25 14:52:29 +0800

    add new

--- RedBall/ViewController.swift
+++ RedBall/ViewController.swift
@@ -8,53 +8,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+let TLspin = "spin"
+let TLmove = "move"
+let TLcolor = "color"
+let TLscale = "scale"
 
+class ViewController: UIViewController {
+    
+    let redBall = UIView(frame: CGRect(x: 100, y: 200, width: 100, height: 100))
+    let scale = POPSpringAnimation(propertyNamed:kPOPViewScaleXY)
+    let move = POPSpringAnimation(propertyNamed: kPOPLayerPositionY)
+    let spin = POPSpringAnimation(propertyNamed: kPOPLayerRotation)
+    let color = POPSpringAnimation(propertyNamed: kPOPViewBackgroundColor)
+    
+ 
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        //创建小红球
-        let redBall = UIView(frame: CGRect(x: 100, y: 100, width: 100, height: 100))
+        
         redBall.backgroundColor = UIColor.red
-        //        redBall.layer.cornerRadius = 50
+        
         self.view.addSubview(redBall)
         
-        // 实例化一个POP
-        let scale = POPSpringAnimation(propertyNamed:kPOPViewScaleXY)
-        //设置结束时的值
-        scale?.toValue = NSValue(cgPoint:CGPoint(x: 2, y: 2))
-        //设置弹性、振幅，范围0-20
-        scale?.springBounciness = 20
-        //设置震动速度
-        scale?.springSpeed = 1
-        //给View加入动画
-        redBall.pop_add(scale, forKey: "tongle")
+        let array = ["放大","旋转","移动","变色","归位"]
         
-        //y坐标位移动画
-        let move = POPSpringAnimation(propertyNamed: kPOPLayerPositionY)
-        move?.toValue = 500
-        move?.springBounciness = 20
-        move?.springSpeed = 5
-        //给layer加入动画
-        redBall.layer.pop_add(move, forKey: "move")
+        let segmentedControl = UISegmentedControl.init(items:array)
         
-        //旋转动画
-        let spin = POPSpringAnimation(propertyNamed: kPOPLayerRotation)
-        spin?.toValue = Double.pi * 4
-        spin?.springBounciness = 20
-        spin?.springSpeed = 5
-        //给layer加入动画
-        redBall.layer.pop_add(spin, forKey: "spin")
+        segmentedControl.frame = CGRect(x:50,y:50,width:self.view.frame.size.width-100 ,height:30)
         
-        //背景颜色变化
-        let color = POPSpringAnimation(propertyNamed: kPOPViewBackgroundColor)
-        color?.toValue = UIColor.green
-        color?.springBounciness = 20
-        color?.springSpeed = 5
-        //给view加入背景颜色变化动画
-        redBall.pop_add(color, forKey: "color")
+        segmentedControl.tintColor = UIColor.green
         
+        segmentedControl.backgroundColor = UIColor.red
         
-
+        segmentedControl.addTarget(self, action:#selector(segmentedControlChanged(segmented:)), for: UIControlEvents.valueChanged)
+        
+        self.view .addSubview(segmentedControl);
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -62,7 +51,85 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    func segmentedControlChanged(segmented : UISegmentedControl) {
+        switch segmented.selectedSegmentIndex {
+        case 0:
+            self.redBallScale()
+        case 1:
+            self.redBallSpin()
+        case 2:
+            self.redBallMove()
+        case 3:
+            self.redBallColor()
+        case 4:
+            self.OriginRedBall()
+        default:
+            print("error");
+        }
+    }
+    
+    /**
+        pop放大，旋转，移动，变色，还原动画
+     1. springBounciness: 弹性振幅，范围0-20
+     2. springSpeed: 震动速度
+     3. pop_add: 加载动画
+     **/
+    
+    func redBallScale()  {
+        // 放大动画
+        scale?.toValue = NSValue(cgPoint:CGPoint(x: 2, y: 2))
+        scale?.springBounciness = 20
+        scale?.springSpeed = 1
+        redBall.pop_add(scale, forKey: TLscale)
+    }
+    func redBallSpin()  {
+        //旋转动画
+        spin?.toValue = Double.pi * 4
+        spin?.springBounciness = 20
+        spin?.springSpeed = 5
+        redBall.layer.pop_add(spin, forKey: TLspin)
+        
+    }
+    func redBallMove()  {
+        //y坐标位移动画
+        move?.toValue = 500
+        move?.springBounciness = 20
+        move?.springSpeed = 5
+        redBall.layer.pop_add(move, forKey: TLmove)
+    }
+    func redBallColor()  {
+        //背景颜色变化
+        color?.toValue = UIColor.green
+        color?.springBounciness = 20
+        color?.springSpeed = 5
+        redBall.pop_add(color, forKey: TLcolor)
+        
+    }
+    func OriginRedBall() {
+        //  scale
+        scale?.toValue = NSValue(cgPoint:CGPoint(x: 1, y: 1))
+        scale?.springBounciness = 20
+        scale?.springSpeed = 1
+        redBall.pop_add(scale, forKey: TLscale)
+        //  move
+        move?.toValue = 200
+        move?.springBounciness = 20
+        move?.springSpeed = 5
+        redBall.layer.pop_add(move, forKey: TLmove)
+        //  spin
+        spin?.toValue = Double.pi * 4
+        spin?.springBounciness = 20
+        spin?.springSpeed = 5
+        redBall.layer.pop_add(spin, forKey: TLspin)
+        // color
+        color?.springBounciness = 20
+        color?.springSpeed = 5
+        color?.toValue = UIColor.red
+        redBall.pop_add(color, forKey: TLcolor)
+    }
+    
+    
 
 }
+
 
